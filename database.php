98fb1968d5fb1ca6d1271897183e699dd97a63b2
GreenyHat <aramirezgarcia95@gmail.com>
2024-03-19 18:59:26 +0100

    link-sql-php

--- database.php
+++ database.php
@@ -1,16 +1,16 @@
 <?php
-$host = "localhost:8888"; //esto solo para MAMPS, en XAMPP, sería sólo localhost
+$host = "localhost";
 $database = "contacts_app";
 $user = "root";
 $password = "";
 
 try {
   $conn = new PDO("mysql:host=$host;dbname=$database", $user, $password);
-  foreach ($conn->query("SHOW DATABASES") as $row) {
-    print_r($row); //como la variable va a ser de dificil lectura, 
-    //con el _r decimos que lo haga string para que sea readible para un humano
-  }
-  die();
+  // foreach ($conn->query("SHOW DATABASES") as $row) {
+  //   print_r($row); //como la variable va a ser de dificil lectura, 
+  //   //con el _r decimos que lo haga string para que sea readible para un humano
+  // }
+  // die(); ESTO ERA PARA PROBAR SI FUNCIONABA
 } catch (PDOException $e) {
   die("PDO Connection Error: " . $e->getMessage());
 }
