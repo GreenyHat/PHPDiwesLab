fe0ffa4bbbb55c761fe010af02a5e499a9aa7cb6
GreenyHat <aramirezgarcia95@gmail.com>
2024-03-21 13:52:29 +0100

    compartmentalized elements

---
+++ partials/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.3.3/darkly/bootstrap.min.css" integrity="sha512-HDszXqSUU0om4Yj5dZOUNmtwXGWDa5ppESlX98yzbBS+z+3HQ8a/7kcdI1dv+jKq+1V5b01eYurE7+yFjw6Rdg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+  <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  <!--El defer que se pone en el script es un atributo de la etiqueta para que cargue antes de que cargue la pagina, en este caso-->
+
+  <!-- Static Content -->
+  <link rel="stylesheet" href="./static/css/index.css" />
+
+  <title>Contacts App</title>
+</head>
+
+<body>
+  <?php require "navbar.php" ?>
+
+  <main>
+    <!-- Content Here -->
